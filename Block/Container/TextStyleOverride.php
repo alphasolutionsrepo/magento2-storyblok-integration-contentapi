5f0982f05c2241c57b7e8552490c170c40b88a33
Roland Villemoes <rv@alpha-solutions.us>
2025-09-01 16:51:19 -0400

    more

--- Block/Container/TextStyleOverride.php
+++ Block/Container/TextStyleOverride.php
@@ -42,7 +42,9 @@ class TextStyleOverride extends Mark
     {
         $this->logger->debug('TextStyleOverride::renderHTML: mark=' . json_encode($mark));
         $this->logger->debug('TextStyleOverride::renderHTML: HTMLAttributes=' . json_encode($HTMLAttributes));
-        $this->logger->debug('TextStyleOverride::renderHTML: isset=' . empty($mark['attrs']));
+        $this->logger->debug('TextStyleOverride::renderHTML: empty($mark)=' . empty($mark));
+        $hasAttrs = isset($mark['attrs']) && !empty($mark['attrs']);
+        $this->logger->debug('TextStyleOverride::renderHTML: has non-empty attrs=' . ($hasAttrs ? 'true' : 'false'));
         $this->logger->debug('TextStyleOverride::renderHTML: is_array=' . is_array($mark['attrs']));
         if (!empty($mark['attrs']) && is_array($mark['attrs'])) {
             $this->logger->debug('TextStyleOverride::renderHTML: ARRAY');
